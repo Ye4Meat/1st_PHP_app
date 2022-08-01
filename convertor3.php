b98e9289227d2e13a94122dce20f53736fdc2427
Ye4Meat <75243932+Ye4Meat@users.noreply.github.com>
2022-08-01 12:29:05 +0500

    Add files via upload
    Add small fixes

--- convertor3.php
+++ convertor3.php
@@ -5,16 +5,19 @@
 </head>
 <body>
     <h1>Преобразование из дюймов в сантиметры.</h1>
-    <form action="convertor3.php" method="get">
+    <form action="convertor3.php" method="post">
         <p>Величина в дюймах:
-        <input type="number" name="inches" size="10"></p>
+        <input type="text" name="inches" size="10"></p>
         <p><input type="submit" value="Преобразовать"></p>
     </form>
     <?php
-        if (isset($_GET['inches'])) {
-            $ins = $_GET['inches'];
-            $cents = round($ins * 2.54);
-            echo '<p>', $ins, ' дюймов = ', $cents, ' сантиметров.</p>';
+        if (isset($_POST['inches'])) {
+            $ins = (double)$_POST['inches'];
+            if ($ins > 0) {
+                $cents = round($ins * 2.54);
+                echo '<p>', $ins, ' дюймов = ', $cents, ' сантиметров.</p>';
+            } else
+                echo '<p>Величина в дюймах должна быть больше нуля.</p>';
         }
     ?>
 </body>
